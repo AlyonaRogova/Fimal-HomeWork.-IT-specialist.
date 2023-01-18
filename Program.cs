16d680c002bb48445b0fd5d11ef920365035ec3b
Alyona Rogova@git config --global user.email rogova.photo@mail.rugit config --global user.name Alyona <rogova.photo@mail.ru>
2023-01-18 18:26:05 +0500

    Added program's file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Итоговая_работа._IT_специалист
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
